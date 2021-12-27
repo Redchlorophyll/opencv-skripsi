aed9e098458d6bdcaca0710ce40af221e554d71f
dahs <hendra.dhonni.888@gmail.com>
2021-12-27 14:30:03 +0700

    remove expressVideo

--- javascript/opencamera/src/components/Camera.vue
+++ javascript/opencamera/src/components/Camera.vue
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+import openCvReady from '@/utils/contrastBrightness';
 
 export default {
     name: 'Camera',
@@ -28,10 +29,10 @@ export default {
     },
     methods: {
         init() {
-            console.log('init clicked')
+            const video = document.getElementById('videoCam');
             this.stream = true;
             this.openCamera();
-            this.openCvReady();
+            openCvReady(video);
         },
         stopAction() {
             console.log('stopp');
@@ -53,105 +54,6 @@ export default {
                     console.log(err, 'cant');
                 });               
             }
-        },
-        brigthness(src, dst) {
-            const cv = window.cv;
-            const gray = cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-            return gray.mean();
-        },
-        openCvReady() {
-            const cv = window.cv;
-            let video = document.getElementById('videoCam');
-            video.height = video.clientHeight;
-            video.width = video.clientHeight;
-            let src = new cv.Mat(video.clientHeight, video.clientWidth, cv.CV_8UC4);
-            let dst = new cv.Mat(video.clientHeight, video.clientWidth, cv.CV_8UC1);
-            let cap = new cv.VideoCapture(video);
-            let brightness = 0;
-            let contrast = 0;
-            const FPS = 30;
-            const stop = this.stream
-
-            function watchContrastAndBrightness(image) {
-                let mean = new cv.Mat;
-                let std = new cv.Mat;
-                image.read(src);
-                cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-                cv.meanStdDev(dst, mean, std);
-
-                return {
-                    getBrightness: mean.data64F[0],
-                    getContrast: std.data64F[0],
-                }
-            }
-
-            function processVideo() {
-                try {
-                    console.log('looping')
-                    
-                    if (stop === false) {
-                        src.delete();
-                        dst.delete();
-                        console.log('berhenti')
-                        return;
-                    } 
-                    let { getBrightness, getContrast } = watchContrastAndBrightness(cap);
-                    cap.read(src);
-                    console.log(getBrightness, getContrast);
-                    console.log(brightness, contrast);
-
-                    if (getBrightness <= 40) {
-                        brightness = 120;
-                        console.log('brightness naik')
-                    } else {
-                        brightness = 20;
-                    }
-                    if (getContrast < 47) {
-                        contrast = getContrast + 20;
-                    }  else {
-                        contrast = 0;
-                    }
-
-                    if (brightness !== 0) {
-                        let shadow = 0;
-                        let highlight = 0;
-                        if (brightness > 0) {
-                            shadow = brightness;
-                            highlight = 255;
-                        } else {
-                            shadow = 0;
-                            highlight = 255 + brightness;
-           }
-                        const alphaB = (highlight - shadow)/255;
-                        const gammaB = shadow;             
-
-                        cv.addWeighted(src, alphaB, src, 0, gammaB, dst);
-                    } else {
-                        dst = src;
-                    }
-
-                    // working on contrast
-                    if (contrast !== 0) {
-                        const f = 131 * (contrast + 127)/(127*(131-contrast));
-                        const alphaC = f;
-                        const gammaC = 127 * (1-f);
-
-                        cv.addWeighted(dst, alphaC, dst, 0, gammaC, dst);
-                    }
-
-                    let begin = Date.now();
-                    // start processing.
-                    // cap.read(src);
-                    
-                    cv.imshow('canvasFrame', dst);
-                    // schedule the next one.
-                    let delay = 1000/FPS - (Date.now() - begin);
-                    setTimeout(processVideo, delay);
-                } catch(err) {
-                    console.error(err);
-                }
-            }
-            setTimeout(processVideo, 0);
         },
     },
     mounted() {
---
+++ javascript/opencamera/src/utils/contrastBrightness.js
@@ -0,0 +1,83 @@
+export default function openCvReady(video) {
+  const cv = window.cv;
+  video.height = video.clientHeight;
+  video.width = video.clientHeight;
+  let src = new cv.Mat(video.clientHeight, video.clientWidth, cv.CV_8UC4);
+  let dst = new cv.Mat(video.clientHeight, video.clientWidth, cv.CV_8UC1);
+  let cap = new cv.VideoCapture(video);
+  let brightness = 0;
+  let contrast = 0;
+  const FPS = 30;
+
+  function watchContrastAndBrightness(image) {
+    let mean = new cv.Mat();
+    let std = new cv.Mat();
+    image.read(src);
+    cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+    cv.meanStdDev(dst, mean, std);
+
+    return {
+      getBrightness: mean.data64F[0],
+      getContrast: std.data64F[0],
+    };
+  }
+
+  function processVideo() {
+    try {
+      let { getBrightness, getContrast } = watchContrastAndBrightness(cap);
+      cap.read(src);
+      console.log(cap);
+      console.log(src);
+      if (getBrightness <= 40) {
+        brightness = 120;
+        console.log("brightness naik");
+      } else {
+        brightness = 20;
+      }
+      if (getContrast < 47) {
+        contrast = getContrast + 20;
+      } else {
+        contrast = 0;
+      }
+
+      if (brightness !== 0) {
+        let shadow = 0;
+        let highlight = 0;
+        if (brightness > 0) {
+          shadow = brightness;
+          highlight = 255;
+        } else {
+          shadow = 0;
+          highlight = 255 + brightness;
+        }
+        const alphaB = (highlight - shadow) / 255;
+        const gammaB = shadow;
+
+        cv.addWeighted(src, alphaB, src, 0, gammaB, dst);
+      } else {
+        dst = src;
+      }
+
+      // working on contrast
+      if (contrast !== 0) {
+        const f = (131 * (contrast + 127)) / (127 * (131 - contrast));
+        const alphaC = f;
+        const gammaC = 127 * (1 - f);
+
+        cv.addWeighted(dst, alphaC, dst, 0, gammaC, dst);
+      }
+
+      let begin = Date.now();
+      // start processing.
+      // cap.read(src);
+
+      cv.imshow("canvasFrame", dst);
+      // schedule the next one.
+      let delay = 1000 / FPS - (Date.now() - begin);
+      setTimeout(processVideo, delay);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  setTimeout(processVideo, 0);
+}
